4aa69fa5d626444685ff38dc7f0e597568febb14
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-20 05:28:14 -0700

    add user authorization spec

---
+++ spec/features/users/user_authorization_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  it 'does not allow me to go to merchant or admin pages' do
+
+    default_user = User.create!(name: "Johnny",
+                                street_address: "123 Jonny Way",
+                                city: "Johnsonville",
+                                state: 'TN',
+                                zip_code: 12345,
+                                email: "roman@example.com",
+                                password: "hamburger01",
+                                role: 0
+                              )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
+
+    expect(default_user.role).to eq("default")
+
+    visit '/admin/dashboard'
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+    visit '/merchant/dashboard'
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+  end
+end
