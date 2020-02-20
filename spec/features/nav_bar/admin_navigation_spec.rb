5375be047d575f28bd0661835a3e4c9fd0f718a2
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-20 03:00:13 -0700

    unskip good test and remove admin needing to see cart

--- spec/features/nav_bar/admin_navigation_spec.rb
+++ spec/features/nav_bar/admin_navigation_spec.rb
@@ -16,14 +16,13 @@ RSpec.describe 'Site Navigation' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin_user)
     end
 
-    xit "sees the regular user links and the merchant dashboard link" do 
+    it "sees the regular user links and the merchant dashboard link" do
 
       visit "/"
 
       within 'nav' do
         expect(page).to have_link("All Merchants")
         expect(page).to have_link("All Items")
-        expect(page).to have_link("Cart")
         expect(page).to have_link("Home")
         expect(page).to have_link("Profile")
         expect(page).to have_link("Logout")
@@ -38,4 +37,3 @@ RSpec.describe 'Site Navigation' do
 
   end
 end
-
