b240f23c465942fe5de4b597a57721b120b4153c
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-19 19:53:46 -0700

    Add the User controller and action methods for the ceration of a new user upon registration
    Co-Authored-By: Chase Gaddis <53872078+cgaddis36@users.noreply.github.com>

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create 
+    user = User.create(user_params)
+    if user.valid?
+        session[:user] = user.id
+        flash[:welcome] = "Welcome, #{user.name}!"
+        flash[:registered] = "You have registered successfully"
+        redirect_to "/profile"
+    else
+        session[:failed_registration] = user_params 
+        flash[:error] = "#{user.errors.full_messages.to_sentence}"
+        redirect_to "/register"
+    end
+  end
+
+  #  def create 
+  #   if user_params[:password] != params[:password_confirmation]
+  #     flash[:notice] = "The password and confirmation password don't match"
+  #     redirect_to "/register"
+  #   else 
+  #     user = User.create(user_params)
+  #     if user.valid?
+  #       session[:user] = user.id
+  #       flash[:welcome] = "Welcome, #{user.name}!"
+  #       flash[:registered] = "You have registered successfully"
+  #       redirect_to "/profile"
+  #     else 
+  #       flash[:error] = "#{user.errors.full_messages.to_sentence}"
+  #       redirect_to "/register"
+  #     end
+  #   end
+  # end
+
+
+  def show 
+
+  end
+
+  private 
+  
+  def user_params 
+    params.permit(:name, :street_address, :city, :state, :zip_code, :email, :password)
+  end
+
+
+end
