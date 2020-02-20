6920a19e59e8cefaa26e44b0c86bb42daf702883
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-19 19:51:13 -0700

    Add the User model and validations
    Co-Authored-By: Chase Gaddis <53872078+cgaddis36@users.noreply.github.com>

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord 
+
+  validates_presence_of :name, :street_address, :city, :state, :zip_code
+  validates_presence_of :password, require: true
+  validates :email, uniqueness: true, presence: true
+
+  validates_confirmation_of :password, :message => "Passwords should match"
+
+  has_secure_password
+
+end
