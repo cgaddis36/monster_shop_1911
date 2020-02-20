d6e5b9278fd5ee56a8158b6ea2f3ccd5076d0f7f
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-20 02:53:24 -0700

    change logic for sessions new view to use global variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,34 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new(user_params)
   end
 
-  def create 
-    user = User.create(user_params)
-    if user.valid?
-      session[:user] = user.id
-      flash[:welcome] = "Welcome, #{user.name}!"
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user] = @user.id
+      flash[:welcome] = "Welcome, #{@user.name}!"
       flash[:registered] = "You have registered successfully"
       redirect_to "/profile"
     else
-      session[:failed_registration] = user_params 
-      flash[:error] = "#{user.errors.full_messages.to_sentence}"
-      redirect_to "/register"
+      flash[:error] = "#{@user.errors.full_messages.to_sentence}"
+      render :new
     end
   end
 
-  #  def create 
+  #  def create
   #   if user_params[:password] != params[:password_confirmation]
   #     flash[:notice] = "The password and confirmation password don't match"
   #     redirect_to "/register"
-  #   else 
+  #   else
   #     user = User.create(user_params)
   #     if user.valid?
   #       session[:user] = user.id
   #       flash[:welcome] = "Welcome, #{user.name}!"
   #       flash[:registered] = "You have registered successfully"
   #       redirect_to "/profile"
-  #     else 
+  #     else
   #       flash[:error] = "#{user.errors.full_messages.to_sentence}"
   #       redirect_to "/register"
   #     end
@@ -36,14 +36,14 @@ class UsersController < ApplicationController
   # end
 
 
-  def show 
+  def show
 
   end
 
-  private 
-  
-  def user_params 
-    params.permit(:name, :street_address, :city, :state, :zip_code, :email, :password)
+  private
+
+  def user_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :email, :password, :password_confirmation)
   end
 
 
