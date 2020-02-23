f3f7a0667b6fb4fe45896a349d57f8b2eb4389b2
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-23 04:08:15 -0700

    add order model methods for merchant

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,4 +15,12 @@ class Order <ApplicationRecord
   def total_quantity
     items.sum(:quantity)
   end
+
+  def item_quantity_merchant(merchant)
+    items.where(merchant: merchant).sum("item_orders.quantity")
+  end
+
+  def item_total_merchant(merchant)
+    items.where(merchant: merchant).sum("item_orders.quantity * item_orders.price")
+  end
 end
