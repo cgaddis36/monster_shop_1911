bc43143def0ff13e622db77b10772bd817507342
Chase.Gaddis <cgaddis36@gmail.com>
2020-02-22 18:56:17 -0700

    Added conditional to change status and increment the items inventory only if the itemorder has status of fulfilled

--- app/controllers/item_orders_controller.rb
+++ app/controllers/item_orders_controller.rb
@@ -4,14 +4,13 @@ class ItemOrdersController < ApplicationController
     order = Order.find(params[:order_id])
     item_orders = order.item_orders.all
     item_orders.each do |itemorder|
-      itemorder.update(status: 0)
-      Item.find(itemorder.id).increment(:inventory, itemorder.quantity).save
+      if itemorder.status == "fulfilled"
+        Item.find(itemorder.id).increment(:inventory, itemorder.quantity).save
+        itemorder.update(status: 0)
+      end
     end
     order.update(status: 3)
     flash[:alert] = "Your order has been cancelled."
     redirect_to '/profile'
   end
-
-
-
 end
