4982525a54fe490f58caa97d752b9b4cb83643a3
Chase.Gaddis <cgaddis36@gmail.com>
2020-02-23 14:22:23 -0700

    deleted in-line comment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,6 @@ class OrdersController <ApplicationController
 
   def create
     order = current_user.orders.create(order_params)
-    # order = Order.create(order_params)
     if order.save
       cart.items.each do |item,quantity|
         order.item_orders.create({
