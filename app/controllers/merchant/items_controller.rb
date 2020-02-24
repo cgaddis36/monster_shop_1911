a6ed08c1bfaadbfa982ad7fc0b65c2301ea82160
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-24 14:25:22 -0700

    Add the index and destroy action methods to the merchant::item controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,6 +1,15 @@
 class Merchant::ItemsController < Merchant::BaseController 
 
   def index
+    @merchant = Merchant.where("id = #{current_user.merchant.id}").first
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    Review.where(item_id: item.id).destroy_all
+    item.destroy
+    flash[:notice] = "#{item.name} has been deleted"
+    redirect_to "/merchant/items"
   end
 
 end
