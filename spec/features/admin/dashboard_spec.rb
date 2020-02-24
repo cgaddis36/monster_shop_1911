20c433dd01a494923e1a1f4cb9cce84bb4e65a3b
KellyIB <kellyibard@gmail.com>
2020-02-23 18:20:59 -0700

    fix test for user story 23

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -17,26 +17,26 @@ require 'rails_helper'
         @pull_toy = @m2.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 200)
         @dog_bone = @m3.items.create(name: "Dog Bone", description: "They'll love it!", price: 20, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 200)
 
-        @order1 = @user1.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
-        @order2 = @user2.orders.create!(name: 'Brian', address: '123 Zanti St', city: 'Denver', state: 'CO', zip: 80204)
-        @order3 = @user3.orders.create!(name: 'Mike', address: '123 Dao St', city: 'Denver', state: 'CO', zip: 80210)
-        @order4 = @user4.orders.create!(name: 'Mike', address: '123 Dao St', city: 'Denver', state: 'CO', zip: 80210)
+        @order1 = @user1.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, status: 0)
+        @order2 = @user2.orders.create!(name: 'Brian', address: '123 Zanti St', city: 'Denver', state: 'CO', zip: 80204, status: 1)
+        @order3 = @user3.orders.create!(name: 'Mike', address: '123 Dao St', city: 'Denver', state: 'CO', zip: 80210, status: 2)
+        @order4 = @user4.orders.create!(name: 'Mike', address: '123 Dao St', city: 'Denver', state: 'CO', zip: 80210, status: 3)
 
-        @order1.item_orders.create!(item: @tire, price: @tire.price, quantity: 22)
-        @order1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 33)
-        @order1.item_orders.create!(item: @dog_bone, price: @dog_bone.price, quantity: 44)
+        @order1.item_orders.create!(item: @tire, price: @tire.price, quantity: 22, status: 0)
+        @order1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 33, status: 0)
+        @order1.item_orders.create!(item: @dog_bone, price: @dog_bone.price, quantity: 44, status: 0)
 
-        @order2.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 22)
-        @order2.item_orders.create!(item: @tire, price: @tire.price, quantity: 33)
-        @order2.item_orders.create!(item: @dog_bone, price: @dog_bone.price, quantity: 44)
+        @order2.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 22, status: 1)
+        @order2.item_orders.create!(item: @tire, price: @tire.price, quantity: 33, status: 1)
+        @order2.item_orders.create!(item: @dog_bone, price: @dog_bone.price, quantity: 44, status: 1)
 
-        @order3.item_orders.create!(item: @tire, price: @tire.price, quantity: 22)
-        @order3.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 33)
-        @order3.item_orders.create!(item: @tire, price: @tire.price, quantity: 44)
+        @order3.item_orders.create!(item: @tire, price: @tire.price, quantity: 22, status: 1)
+        @order3.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 33, status: 1)
+        @order3.item_orders.create!(item: @tire, price: @tire.price, quantity: 44, status: 1)
 
-        @order4.item_orders.create!(item: @tire, price: @tire.price, quantity: 3)
-        @order4.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 6)
-        @order4.item_orders.create!(item: @tire, price: @tire.price, quantity: 9)
+        @order4.item_orders.create!(item: @tire, price: @tire.price, quantity: 3, status: 0)
+        @order4.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 6, status: 0)
+        @order4.item_orders.create!(item: @tire, price: @tire.price, quantity: 9, status: 0)
 
         @admin_user = User.create!(name: "Bob T Builder",
                                     street_address: "123 Contractor Blvd.",
@@ -50,28 +50,34 @@ require 'rails_helper'
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin_user)
       end
 
-      it "sees all orders in the system and the order's user name links to the admin view of the user" do
+      it "sees all orders sorted by status and the order's user name links to the admin view of the user" do
         visit "/admin"
 
-         within ".orders" do
+         within ".pending-orders" do
           within "#order-#{@order1.id}" do
             expect(page).to have_content("Order ID: #{@order1.id}")
             expect(page).to have_content(@order1.created_at)
             expect(page).to have_link(@order1.user.name)
           end
+        end
 
+        within ".packaged-orders" do
            within "#order-#{@order2.id}" do
             expect(page).to have_content("Order ID: #{@order2.id}")
             expect(page).to have_content(@order2.created_at)
             expect(page).to have_link(@order2.user.name)
           end
+        end
 
+        within ".shipped-orders" do
           within "#order-#{@order3.id}" do
             expect(page).to have_content("Order ID: #{@order3.id}")
             expect(page).to have_content(@order3.created_at)
             expect(page).to have_link(@order3.user.name)
           end
+        end
 
+        within ".cancelled-orders" do
           within "#order-#{@order4.id}" do
             expect(page).to have_content("Order ID: #{@order4.id}")
             expect(page).to have_content(@order4.created_at)
@@ -81,22 +87,5 @@ require 'rails_helper'
 
         expect(current_path).to eq("/admin/users/#{@order4.user.id}")
     end
-
-    xit "sorts the orders by order status (packaged, pending, shipped, cancelled)" do
-      @order1.status = "packaged"
-      @order2.status = "pending"
-      @order3.status = "shipped"
-      @order4.status = "cancelled"
-
-      # binding.pry
-
-
-    end
   end
 end
-# Orders are sorted by "status" in this order:
-#
-# - packaged
-# - pending
-# - shipped
-# - cancelled
