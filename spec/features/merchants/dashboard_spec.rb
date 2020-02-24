3e8edcc7d5a27325c0f026612db47b7d63558918
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-23 17:15:17 -0700

    add merchant user to merchant to fix test

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "As a merchant employee", type: :feature do 
+RSpec.describe "As a merchant employee", type: :feature do
 
-  describe 'when I visit my merchant dashboard' do 
+  describe 'when I visit my merchant dashboard' do
 
-    before(:each) do 
+    before(:each) do
       @merchant_user = User.create!(name: "Johnny",
                                   street_address: "123 Jonny Way",
                                   city: "Johnsonville",
@@ -14,17 +14,21 @@ RSpec.describe "As a merchant employee", type: :feature do
                                   password: "hamburger01",
                                   role: 1
                                 )
-      
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+
+      @bike_shop.users << @merchant_user
+
+
     end
 
-    it 'has a link to view the merchant items' do 
+    it 'has a link to view the merchant items' do
 
       visit "/login"
 
       fill_in :email, with: @merchant_user.email
       fill_in :password, with: @merchant_user.password
       click_on "Log In"
-      visit '/merchant/dashboard'
+      visit '/merchant'
       click_on("My Items")
       expect(current_path).to eq("/merchant/items")
     end
