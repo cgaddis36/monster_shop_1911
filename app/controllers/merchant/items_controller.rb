aba9d4d450cab0fa97accc0a61cfe9833c66800a
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-24 18:55:17 -0700

    add update method and update method helper for flash message

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,7 +1,19 @@
-class Merchant::ItemsController < Merchant::BaseController 
+class Merchant::ItemsController < Merchant::BaseController
 
   def index
     @merchant = Merchant.where("id = #{current_user.merchant.id}").first
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if params[:commit]
+      @item.update(item_params)
+      give_default_image_if_needed(@item)
+      edit_item_info(@item)
+    else
+      @item.switch_active_status
+      switch_active_with_flash(@item)
+    end
   end
 
   def destroy
@@ -10,6 +22,15 @@ class Merchant::ItemsController < Merchant::BaseController
     item.destroy
     flash[:notice] = "#{item.name} has been deleted"
     redirect_to "/merchant/items"
+  end
+
+  def switch_active_with_flash(item)
+    if item.active?
+      flash[:success] = "#{item.name} is activated"
+    else
+      flash[:success] = "#{item.name} is deactivated"
+    end
+    redirect_to '/merchant/items'
   end
 
 end
