e8e4ad64edf91ee12e1b8ba02b8a6dad3c3991d8
KellyIB <kellyibard@gmail.com>
2020-02-25 00:08:08 -0700

    test for user story 53, added routes and view for admin to see all users

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,11 @@
+class Admin::OrdersController < Admin::BaseController
+
+  def update
+    order = Order.find(params[:order_id])
+    if params[:status_change]  == "ship"
+      order.update_column(:status, "shipped")
+      flash[:alert] = "The order has been shipped."
+    end
+    redirect_to "/admin"
+  end
+end
