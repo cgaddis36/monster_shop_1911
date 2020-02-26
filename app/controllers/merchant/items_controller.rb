c12f14d58f9c30f185cb323a89ac8712f18fc6ac
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-26 13:04:21 -0700

    Refactor the merchant items controller to move the core of the action methods to private and make the controller skinnier (Mike's recommandation)

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -14,23 +14,12 @@ class Merchant::ItemsController < Merchant::BaseController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      flash[:notice] = 'Your item was updated successfully'
-      redirect_to "/merchant/items"
-    else
-      session[:failed_update] = params[:id]
-      flash[:error] = @item.errors.full_messages.to_sentence
-      redirect_to "/merchant/items/#{params[:id]}/edit"
-    end
+    update_item(@item)
   end
 
   def destroy
     item = Item.find(params[:id])
-    Review.where(item_id: item.id).destroy_all
-    item.destroy
-    flash[:notice] = "#{item.name} has been deleted"
-    redirect_to "/merchant/items"
+    destroy_item(item)
   end
 
   def new
@@ -42,8 +31,39 @@ class Merchant::ItemsController < Merchant::BaseController
   end
 
   def create
-    merchant = Merchant.where("id = #{current_user.merchant.id}").first
+    merchant = Merchant.find(current_user.merchant.id)
     @item = merchant.items.create(item_params)
+    create_item(@item)
+  end
+  
+  private 
+  
+  def item_params
+    params.require(:item).permit(:name, :price, :age, :description, :image, :inventory)
+  end
+
+  def update_item(item)
+    @item = item
+    @item.update(item_params)
+    if @item.save
+      flash[:notice] = 'Your item was updated successfully'
+      redirect_to "/merchant/items"
+    else
+      session[:failed_update] = params[:id]
+      flash[:error] = @item.errors.full_messages.to_sentence
+      redirect_to "/merchant/items/#{params[:id]}/edit"
+    end
+  end
+
+  def destroy_item(item)
+    Review.where(item_id: item.id).destroy_all
+    item.destroy
+    flash[:notice] = "#{item.name} has been deleted"
+    redirect_to "/merchant/items"
+  end
+
+  def create_item(item)
+    @item = item
     if @item.save
       flash[:notice] = 'Your new item was saved'
       redirect_to "/merchant/items"
@@ -52,12 +72,6 @@ class Merchant::ItemsController < Merchant::BaseController
       flash[:error] = @item.errors.full_messages.to_sentence
       redirect_to "/merchant/items/new"
     end
-  end
-  
-  private 
-  
-  def item_params
-    params.require(:item).permit(:name, :price, :age, :description, :image, :inventory)
   end
 
 end
