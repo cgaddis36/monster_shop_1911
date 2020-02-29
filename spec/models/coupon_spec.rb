53cb38b4ddeab100caa14441f88563c4a9ee3a84
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-29 03:40:46 -0700

    add coupon spec

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Coupon, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_inclusion_of(:value).in_array((0..100).to_a).with_message("value must be between 0 and 100.")}
+  end
+
+  describe "relationships" do
+    it {should belong_to :merchant}
+    it {should have_many :orders}
+    binding.pry
+  end
+
+end
