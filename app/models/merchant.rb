f1612e2dff07185a24a423027461c5878d6b5264
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-29 03:41:50 -0700

    add merchants having many coupons

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,7 @@ class Merchant <ApplicationRecord
   has_many :item_orders, through: :items
   has_many :users
   has_many :orders, through: :item_orders
+  has_many :coupons
   enum status: %w(enabled disabled)
 
 
