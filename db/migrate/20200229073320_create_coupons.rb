0bee6d0d91f89898b6c6324cf5be4e226036c88b
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-29 18:40:52 -0700

    add item_quantity to coupons table

--- db/migrate/20200229073320_create_coupons.rb
+++ db/migrate/20200229073320_create_coupons.rb
@@ -3,6 +3,7 @@ class CreateCoupons < ActiveRecord::Migration[5.1]
     create_table :coupons do |t|
       t.string :name
       t.float :value
+      t.integer :item_quantity
       t.references :merchant, foreign_key: true
 
       t.timestamps
