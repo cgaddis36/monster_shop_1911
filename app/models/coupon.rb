d7447a45545c8858e7b3be97a751aa68214b5357
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-29 18:41:29 -0700

    add new validation for item_quantity

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,7 +1,7 @@
 class Coupon < ApplicationRecord
-  validates_presence_of :name
+  validates_presence_of :name, :item_quantity
   validates_inclusion_of :value, in: 0..100, message: "value must be between 0 and 100."
 
-  belongs_to :merchant 
+  belongs_to :merchant
   has_many :orders
 end
