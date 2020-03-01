39f57f2a34399a987d071acec96fd869446076f5
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-29 18:40:58 -0700

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20200229081915) do
   create_table "coupons", force: :cascade do |t|
     t.string "name"
     t.float "value"
+    t.integer "item_quantity"
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
