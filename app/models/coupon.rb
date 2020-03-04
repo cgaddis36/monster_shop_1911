30e5101190fc2ff300dc8ad68d5607c755a016de
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-03 21:29:17 -0700

    update coupon model

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,6 @@
 class Coupon < ApplicationRecord
   validates_presence_of :name, :item_quantity
-  validates_inclusion_of :value, in: 0..100, message: "value must be between 0 and 100."
+  validates_inclusion_of :value, in: 0..100, message: "must be between 0 and 100."
 
   belongs_to :merchant
   has_many :orders
