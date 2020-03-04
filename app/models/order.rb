080fbc308bd95423f6ab224634985af6e5789fe4
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-03 23:39:28 -0700

    add new logic to grand total calculation on index view

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,23 @@ class Order < ApplicationRecord
   enum status: %w[pending packaged shipped cancelled]
 
   def grandtotal
-    item_orders.sum('price * quantity')
+    grand_total = 0
+    item_orders.each do |item_order|
+      item = Item.find(item_order.item_id)
+      merchant = Merchant.find(item.merchant_id)
+      number_ordered = item_order.quantity
+      best_coupon = merchant.coupons.where("coupons.item_quantity <= ?", number_ordered).first
+      if best_coupon != nil
+        regular_price = item.price * item_order.quantity
+        discount = (100 - best_coupon.value) / 100
+        new_cost = regular_price * discount
+        grand_total += new_cost
+      else
+        reg_cost = item_order.price * item_order.quantity
+        grand_total += reg_cost
+      end
+    end
+    grand_total
   end
 
   def total_quantity
