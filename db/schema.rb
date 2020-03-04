5a63a40106e70eda65e559c254396c73ad1337f6
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-03 21:33:18 -0700

    udpdate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200229081915) do
+ActiveRecord::Schema.define(version: 20200304024423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20200229081915) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "coupon_id"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
