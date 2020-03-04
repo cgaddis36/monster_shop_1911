f8dec0c8e650d4b9b23f81e9b36772ec8a6b7485
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-03 23:39:52 -0700

    add new subtotal logic to order show page view

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -7,6 +7,19 @@ class ItemOrder <ApplicationRecord
   enum status: %w(unfulfilled fulfilled)
 
   def subtotal
-    price * quantity
+    grand_total = 0
+    merchant = Merchant.find(item.merchant_id)
+    number_ordered = quantity
+    best_coupon = merchant.coupons.where("coupons.item_quantity <= ?", number_ordered).first
+    if best_coupon != nil
+      regular_price = item.price * quantity
+      discount = (100 - best_coupon.value) / 100
+      new_cost = regular_price * discount
+      grand_total += new_cost
+    else
+      reg_cost = item.price * quantity
+      grand_total += reg_cost
+    end
+    grand_total
   end
 end
