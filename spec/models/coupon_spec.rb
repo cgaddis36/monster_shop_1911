aa9b9e9566ab5cd032968270667813b59865a498
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-03 21:28:45 -0700

    update coupon spec

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -4,7 +4,7 @@ describe Coupon, type: :model do
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :item_quantity}
-    it {should validate_inclusion_of(:value).in_array((0..100).to_a).with_message("value must be between 0 and 100.")}
+    it {should validate_inclusion_of(:value).in_array((0..100).to_a).with_message("must be between 0 and 100.")}
   end
 
   describe "relationships" do
